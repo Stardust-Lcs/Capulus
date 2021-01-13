2fbae2a28364e6c65bd617ae4dcde134abce4f20
cloudenum <23111954+cloudenum@users.noreply.github.com>
2021-01-14 05:12:47 +0700

    fix login

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -13,8 +13,8 @@ class Auth {
             if (isset($_POST['email']) && isset($_POST['password'])) {
                 $email = htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');
                 $pass = htmlentities($_POST['password'], ENT_QUOTES, 'UTF-8');
-                $row = $user->get([], "email = '$email'");
-
+                $row = $user->get(['user_id', 'username', 'password'], "email = '$email'");
+                readable_var_dump($row);
                 if ($row) {
                     if (password_verify($pass, $row[0]->password)) {
                         $session->set('user_id', $row[0]->user_id);
