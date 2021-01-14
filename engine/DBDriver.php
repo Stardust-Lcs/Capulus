9a394fac7f18b517dfc17ec724b8734d8cebf8a7
cloudenum <23111954+cloudenum@users.noreply.github.com>
2021-01-14 07:57:55 +0700

    persistent db connection

--- engine/DBDriver.php
+++ engine/DBDriver.php
@@ -5,12 +5,12 @@ class DBDriver {
     public static $username;
     public static $pass;
 
-    public static function getDBConn() {
-        return self::$DBConn;
-    }
-
     public static function initialize() {
-        self::$DBConn = new PDO(self::$dsn, self::$username, self::$pass);
-        self::$DBConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if (self::$DBConn === null) {
+            self::$DBConn = new PDO(self::$dsn, self::$username, self::$pass, array(
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ));
+        }
     }
 }
